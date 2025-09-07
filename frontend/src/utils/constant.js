c3aa88a4c7522f6c5232b835ff552fde862872ab
arindampattanayak <65799553+arindampattanayak@users.noreply.github.com>
2025-09-07 12:41:41 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,7 +1,6 @@
-// Get the API base URL from environment variables
+
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
-// Build endpoints dynamically
 export const USER_API_END_POINT = `${API_BASE_URL}/api/v1/user`;
 export const JOB_API_END_POINT = `${API_BASE_URL}/api/v1/job`;
 export const APPLICATION_API_END_POINT = `${API_BASE_URL}/api/v1/application`;
